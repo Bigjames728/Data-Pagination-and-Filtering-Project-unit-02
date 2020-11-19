7a402ecadb2264ead0c3a8185e6e798e5d02f489
Bigjames728 <bigjames728@gmail.com>
2020-11-19 15:50:05 -0800

    Final commit

--- js/script.js
+++ js/script.js
@@ -4,13 +4,6 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
 // Select the header class from the index file
 
 let searchArea = document.querySelector('.header');
